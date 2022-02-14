077bc705e5b50e3f64ab3df08bde0138dbac67ca
Sahil Kumar <sahilkumar142002@gmail.com>
2022-02-14 17:10:33 +0530

    +enroll now & view courses buttons

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ document.querySelector('#login-btn').onclick = () =>{
   document.querySelector('.login-form-container').classList.toggle('active');
 }
 
+function login() {
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
 document.querySelector('#close-login-form').onclick = () =>{
   document.querySelector('.login-form-container').classList.remove('active');
 }
