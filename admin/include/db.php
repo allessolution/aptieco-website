6a2fb04048870f933d82f74777f4f1288121f10c
Sahil Kumar <sahilkumar142002@gmail.com>
2022-02-23 00:32:10 +0530

    updated db.php

--- admin/include/db.php
+++ admin/include/db.php
@@ -8,6 +8,6 @@ $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // echo "Connected successfully";
 } catch(PDOException $e) {
-echo "Connection failed: " . $e->getMessage();
+// echo "Connection failed: " . $e->getMessage();
 }
 ?>
