3bec4b877d2be1270a5fa5a034e04a30171cb9e1
Sahil Kumar <sahilkumar142002@gmail.com>
2022-02-22 20:48:29 +0530

    added tinymce editor

--- admin/include/header.php
+++ admin/include/header.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- tiny mce js -->
+    <script src="https://cdn.tiny.cloud/1/nr3rxl4o16xogtfi1v830uczacri9682tyt2zj6diyjztojl/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <!-- custom css -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
