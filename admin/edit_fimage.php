2ae502b592c2758231c323975f53669cb38da68a
Sahil Kumar <sahilkumar142002@gmail.com>
2022-02-26 15:25:16 +0530

    some minor chnages

--- admin/edit_fimage.php
+++ admin/edit_fimage.php
@@ -66,7 +66,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
         $f_image
       ));
       alert2('Featured Image Updated Sucessfully','success');
-      header( "refresh:1;url=courses.php" );
     } else {
       alert2("Sorry, there was an error in uploading data",'danger');
     }
